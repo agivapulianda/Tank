1f85212a420fd299725993066e277f396d1fd01e
agivapulianda <agivapulianda@gmail.com>
2020-06-24 14:03:25 +0700

    Video 8

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    public GameObject go_sudutMeriam;
+    public GameObject go_sudutTembak;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
